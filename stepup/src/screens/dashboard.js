9f051cb6167a37fa08b01698dbb800c6ffbad2ef
shoryataneja <shoryataneja5@gmail.com>
2025-11-28 23:34:24 +0530

    Fixing the double navigation issue

--- stepup/src/screens/dashboard.js
+++ stepup/src/screens/dashboard.js
@@ -20,7 +20,7 @@ const Dashboard = () => {
                 <WeeklyGoals />
                 <RecentWorkouts />
             </ScrollView>
-            <BottomNav />
+            {/* <BottomNav /> */}
         </View>
     );
 };
